c4ec04c5409f6023eff62ea6cee32c1d928d9f0a
nestier <fernan.nestier@gmail.com>
2016-06-22 21:02:21 -0300

    create querys for search

--- couchInn/app/lodgment/query.py
+++ couchInn/app/lodgment/query.py
@@ -2,12 +2,46 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.core.urlresolvers import reverse
 from django.contrib import messages
 from .models import Lodgment, Place, Request
+from .forms import SearchForm
 from django.db.models import Q
+import datetime
 
 def simple_query(request):
-    param = request.GET['params']
+    get = request.GET
+    param =get.get('params', False)
     if param:
         lodgments=Lodgment.actives.filter(Q(place__city=param) | Q(title=param) | Q(author__username=param)| Q(category__name=param))
     else:
         lodgments=Lodgment.actives.all()
     return render(request,'lodgment/index.html',{'lodgments':lodgments, 'params':param})
+
+
+def advance_query(request):
+    get = request.GET
+    category = get.get('category', False)
+    city = get.get('city', False)
+    score = get.get('score', False)
+    province = get.get('province', False)
+    initial_date = get.get('initial_date', False)
+    finish_date = get.get('finish_date', False)
+    form = SearchForm(request.GET or None)
+    lodgments = Lodgment.actives.all()
+    if form.is_valid():
+        if score:
+            lodgments = lodgments.filter(place__score=score)
+        if city:
+            lodgments = lodgments.filter(place__city=city)
+        if province:
+            lodgments = lodgments.filter(place__province=province)
+        if category:
+            lodgments = lodgments.filter(category=category)
+        if finish_date:
+            finish_date = datetime.datetime.strptime(finish_date, "%d/%m/%Y").strftime("%Y-%m-%d")
+            lodgments = lodgments.filter(initial_date__lte=finish_date).order_by('initial_date')
+        else:
+            finish_date = datetime.date.today()
+        if initial_date:
+            initial_date = datetime.datetime.strptime(initial_date, "%d/%m/%Y").strftime("%Y-%m-%d")
+            lodgments = lodgments.filter(finish_date__range=(initial_date,finish_date))
+        return render(request,'lodgment/detail_search.html',{'lodgments':lodgments,'form':form})
+    return render(request,'lodgment/detail_search.html',{'lodgments':lodgments, 'form':form})
