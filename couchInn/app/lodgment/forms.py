00973a4825e36740569837fce975214372129cc2
nestier <fernan.nestier@gmail.com>
2016-06-22 21:00:38 -0300

    create search form

--- couchInn/app/lodgment/forms.py
+++ couchInn/app/lodgment/forms.py
@@ -1,6 +1,7 @@
 import datetime
 from django import forms
 from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator, MinValueValidator 
 
 from crispy_forms.helper import FormHelper
 
@@ -87,3 +88,41 @@ class RequestForm(forms.ModelForm):
             print e
 
         return cleaned_data
+
+class SearchForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super(SearchForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_tag = False
+
+    initial_date = forms.DateField(label='Fecha de inicio',input_formats=['%d/%m/%Y'], widget= forms.DateInput(attrs={'class':'datepicker'}),required=False)
+    finish_date = forms.DateField(label='Fecha de fin',input_formats=['%d/%m/%Y'], widget= forms.DateInput(attrs={'class':'datepicker'}),required=False)
+    category = forms.ModelChoiceField(queryset=Category.actives.all(), label='Tipo de hospedaje',required=False)
+    city = forms.CharField(label='Ciudad',max_length=100,required=False)
+    province = forms.CharField(label='Provincia',max_length=100,required=False)
+    score = forms.FloatField(label='Valoracion', 
+        validators = [
+            MaxValueValidator(5),
+            MinValueValidator(0)
+            ]
+        ,required=False
+        )
+    
+    def clean(self):
+        print 'saraarararararasraarra'
+        cleaned_data = super(SearchForm, self).clean()
+        print cleaned_data
+        try:
+            today = datetime.date.today()
+            init = cleaned_data.get('initial_date')
+            end = cleaned_data.get('finish_date')
+            if today >= init or today >= end:
+                raise forms.ValidationError('Las fechas tiene que ser mayores que la actual')
+            if end <= init :
+                raise forms.ValidationError('La fecha de fin tiene que ser mayor que la de inicio')
+        except TypeError as e:
+            print e
+
+        return cleaned_data
+   
