65710d949aee4cc0b965b86afa93c34e0b4c32c4
Matias Gudin <stkboy@gmail.com>
2016-07-13 14:36:38 -0300

    agrega busqueda de donaciones

--- couchInn/app/backend/forms.py
+++ couchInn/app/backend/forms.py
@@ -3,7 +3,7 @@ from django import forms
 from django.core.exceptions import ValidationError
 from models import Category
 from crispy_forms.helper import FormHelper
-
+import datetime
 
 class CategoryForm(forms.ModelForm):
 
@@ -16,3 +16,24 @@ class CategoryForm(forms.ModelForm):
         model = Category
         fields = ['name']
 
+
+class DonationFilterForm(forms.Form):
+    initial_date = forms.DateField(label='Fecha de inicio',input_formats=['%d/%m/%Y'], widget= forms.DateInput(attrs={'class':'datepicker'}))
+    finish_date = forms.DateField(label='Fecha de fin',input_formats=['%d/%m/%Y'], widget= forms.DateInput(attrs={'class':'datepicker'}))
+
+    def __init__(self, *args, **kwargs):
+        super(DonationFilterForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_tag = False
+
+    
+    def clean(self):
+        cleaned_data = super(DonationFilterForm, self).clean()
+        # try:
+        #     today = datetime.date.today()
+        init = cleaned_data.get('initial_date')
+        end = cleaned_data.get('finish_date')
+        if end <= init :
+            raise forms.ValidationError('La fecha de fin tiene que ser mayor que la de inicio')
+            
+        return cleaned_data
